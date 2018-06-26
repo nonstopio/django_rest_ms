494c164e5ac26239d101f42c655c75ffbfd8f61f
Arnold Parge <pargearnold@gmail.com>
2018-06-26 15:20:16 +0530

    updated zero config, minor bug fixed

--- player_ms/player_app/models.py
+++ player_ms/player_app/models.py
@@ -20,16 +20,3 @@ class PlayerGoal(models.Model):
 
     def __str__(self):
         return '%s' % self.id
-
-
-2931
-1
-0
-2: 35
-PM ??         0: 18.38 / Users / nonstopio / workspace / microservices / worldcup / env_worldcup / bin / python3. / match_ms / manage.py
-runserver
-127.0
-.0
-.1: 7003
-501
-2932
--- zero_config.bash
+++ zero_config.bash
@@ -1,6 +1,6 @@
 virtualenv env_worldcup -p python3
 source env_worldcup/bin/activate
-pip install requirements.txt
+pip install -r requirements.txt
 python ./team_ms/manage.py runserver 127.0.0.1:7001 &
 python ./player_ms/manage.py runserver 127.0.0.1:7002 &
 python ./match_ms/manage.py runserver 127.0.0.1:7003 &
