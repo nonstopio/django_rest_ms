0eb5dbca8f5c055b0feef80245596ab9686f5cc2
Arnold Parge <pargearnold@gmail.com>
2018-07-02 17:17:39 +0530

    updated master files and folders

--- player_ms/player_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from player_app.models import Player
-from import_export.admin import ImportExportMixin
-
-
-@admin.register(Player)
-class PlayerAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ('id', 'name', 'image', 'team_id', 'high_score')
-    search_fields = ('id', 'name', 'image', 'team_id', 'high_score')
