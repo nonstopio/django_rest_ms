eb0f9b53445425b26c66b02a03f96a1aae839ad0
Arnold Parge <pargearnold@gmail.com>
2018-07-02 19:39:32 +0530

    updated urls

--- player_app/services/common.py
+++ player_app/services/common.py
@@ -3,6 +3,6 @@ class MSUrls:
     # team = '%s:7001/' % base
     # player = '%s:7002/' % base
     # match = '%s:7003/' % base
-    team = 'http://team_web:7001/'
-    player = 'http://player_web:7002/'
-    match = 'http://match_web:7003/'
+    team = 'http://teamweb:7001/'
+    player = 'http://playerweb:7002/'
+    match = 'http://matchweb:7003/'
