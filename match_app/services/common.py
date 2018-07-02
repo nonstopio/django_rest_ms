429f6b6b8fa67516fbb25892acfcbf4139492dbc
Arnold Parge <pargearnold@gmail.com>
2018-07-02 19:03:42 +0530

    updated common url. added web

--- match_app/services/common.py
+++ match_app/services/common.py
@@ -3,6 +3,6 @@ class MSUrls:
     # team = '%s:7001/' % base
     # player = '%s:7002/' % base
     # match = '%s:7003/' % base
-    team = 'http://team_ms:7001/'
-    player = 'http://player_ms:7002/'
-    match = 'http://match_ms:7003/'
+    team = 'http://team_web:7001/'
+    player = 'http://player_web:7002/'
+    match = 'http://match_web:7003/'
