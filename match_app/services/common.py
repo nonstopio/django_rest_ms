64f8c2aafd0b8a325bd1cc277a7155ed5caa4683
Arnold Parge <pargearnold@gmail.com>
2018-07-02 17:36:15 +0530

    updated common urls

--- match_app/services/common.py
+++ match_app/services/common.py
@@ -1,5 +1,8 @@
 class MSUrls:
-    base = 'http://192.168.1.5'
-    team = '%s:7001/' % base
-    player = '%s:7002/' % base
-    match = '%s:7003/' % base
+    # base = 'http://192.168.1.5'
+    # team = '%s:7001/' % base
+    # player = '%s:7002/' % base
+    # match = '%s:7003/' % base
+    team = 'http://team_ms:7001/'
+    player = 'http://player_ms:7002/'
+    match = 'http://match_ms:7003/'
